b71ddb65d97bfa3a1c5a22a61574dc1f45258451
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-05-18 21:42:29 +0700

    feat: Logout on Profile

--- app/src/main/java/com/gunadarma/rsug/ProfileActivity.java
+++ app/src/main/java/com/gunadarma/rsug/ProfileActivity.java
@@ -2,13 +2,30 @@ package com.gunadarma.rsug;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button btnLogout = findViewById(R.id.btn_move_activity_login);
+        btnLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_move_activity_login) {
+            Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+            intent.putExtra("finish", true);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
     }
 }
